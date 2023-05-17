70e334889a8c4f17c6fad9e28b988fa21998e6f2
DE204-08 <DE204-08@techno-lab.ca>
2023-05-17 14:15:38 -0400

    exo2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { SecondComponent } from './components/second.component';
 import { Exo1Component } from './components/exo1/exo1.component';
+import { Exo2Component } from './components/exo2/exo2.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
     SecondComponent,
-    Exo1Component
+    Exo1Component,
+    Exo2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [],
   providers: [],
---
+++ src/app/components/exo2/exo2.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exo2',
+  templateUrl: './exo2.component.html',
+  styleUrls: ['./exo2.component.css']
+})
+export class Exo2Component {
+  name: string = "Bolduc";
+  firstname: string = "Samuel";
+  job: string = "Tech Lead";
+  path: string = "/assets/images/rotating_card_profile3.png";
+}
